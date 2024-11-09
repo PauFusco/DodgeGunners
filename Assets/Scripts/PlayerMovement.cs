e2398884fb00294903c7b1e90f95133863783a63
Júlia Serra Trujillo <juliaserratrujillo@gmail.com>
2024-11-10 00:10:18 +0100

    Basic PlayerMovement to debug

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    float m_speed = 2.0f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,18 +15,14 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-
-        }
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-
-        }
+        if (Input.GetKey(KeyCode.A))
+            transform.position += Vector3.left * m_speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.D))
+            transform.position += Vector3.right * m_speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.W))
+            transform.position += Vector3.forward * m_speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.S))
+            transform.position += Vector3.back * m_speed * Time.deltaTime;
 
     }
 }
