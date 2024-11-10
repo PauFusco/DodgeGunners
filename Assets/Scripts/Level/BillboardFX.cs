1463657d96116a6fb66d69cfd16ec4a4a199a2b6
Júlia Serra Trujillo <juliaserratrujillo@gmail.com>
2024-11-10 23:47:23 +0100

    Update BillboardFX.cs

--- Assets/Scripts/Level/BillboardFX.cs
+++ Assets/Scripts/Level/BillboardFX.cs
@@ -4,13 +4,6 @@ using UnityEngine;
 
 public class BillboardFX : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         transform.forward = Camera.main.transform.forward;
