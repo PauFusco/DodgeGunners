72ff1106d074d3bb9a15f99744373b7aee785073
Júlia Serra Trujillo <juliaserratrujillo@gmail.com>
2024-11-16 20:53:20 +0100

    Created bullet prefab
    - Created bullet prefab
    - Created script for bullet
    - Created two folders (bullet/player)

---
+++ Assets/Scripts/Level/Bullet/Bullet.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Bullet
+{
+    class Bullet
+    {
+        public float _lifeTime;
+        public Vector3 _position;
+        public float _speed;
+        public Vector3 _velocity;
+        public float _damage; //Necessary?
+    }
+    
+}
