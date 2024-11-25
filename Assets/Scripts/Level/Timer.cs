8abf3c6eefc2c269875f446c331e88a58c9f672f
Júlia Serra Trujillo <juliaserratrujillo@gmail.com>
2024-11-25 10:18:41 +0100

    Update Timer.cs
    - Changed Timer to Countdown
    - Updated it to show only minutes and seconds

--- Assets/Scripts/Level/Timer.cs
+++ Assets/Scripts/Level/Timer.cs
@@ -7,24 +7,17 @@ using System;
 public class Timer : MonoBehaviour
 {
     private TMP_Text m_TimerText;
-    private float m_Time;
+    public float m_Time;
 
     void Awake()
     {
         m_TimerText = GetComponent<TMP_Text>();
     }
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
-        m_Time += Time.deltaTime;
+        m_Time -= Time.deltaTime;
 
-        m_TimerText.text = TimeSpan.FromSeconds(m_Time).ToString(format: @"mm\:ss\:ff");
+        m_TimerText.text = TimeSpan.FromSeconds(m_Time).ToString(format: @"mm\:ss");
     }
 }
