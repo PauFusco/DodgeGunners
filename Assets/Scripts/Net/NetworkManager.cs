df60c4ccc6a20c66428918dbbfa2991f922abaab
PauFusco <99730051+PauFusco@users.noreply.github.com>
2024-12-14 15:42:15 +0100

    network manager
    added a packet type enum to make the remote know what type of packet they are receiving

--- Assets/Scripts/Net/NetworkManager.cs
+++ Assets/Scripts/Net/NetworkManager.cs
@@ -15,12 +15,23 @@ public class NetworkManager : MonoBehaviour
     private ProjectileController projectileController;
     private GameManager gameManager;
 
+    private enum PacketType
+    {
+        PLAYER,
+        PROJECTILE,
+        DEFAULT
+    }
+
     public class PlayerPacket
     {
         public PlayerPacket(GameManager.NetPlayer playerToSend, PlayerBehaviour playerBHToSend)
         {
+            _type = PacketType.PLAYER;
+
             MemoryStream playerMStream = new();
             BinaryWriter playerBWriter = new(playerMStream);
+
+            playerBWriter.Write(_type.ToString());
 
             // flags to byte
             // frame to byte
@@ -44,15 +55,12 @@ public class NetworkManager : MonoBehaviour
             _data = playerMStream.ToArray();
         }
 
-        /// <summary>
-        /// Creates a packet using a buffer and modifies the player set in the definition
-        /// </summary>
-        /// <param name="data"></param>
-        /// <param name="playerToModify"></param>
         public PlayerPacket(byte[] data, GameManager.NetPlayer playerToModify, PlayerBehaviour playerBHToModify)
         {
             MemoryStream playerMStream = new(data);
             BinaryReader playerBReader = new(playerMStream);
+
+            _type = (PacketType)playerBReader.Read();
 
             // frame
             // flags
@@ -82,6 +90,7 @@ public class NetworkManager : MonoBehaviour
         private readonly byte[] _data;
         private Vector3 _position = new();
         private Quaternion _rotation = new();
+        private PacketType _type;
 
         public byte[] GetBuffer()
         { return _data; }
@@ -115,8 +124,12 @@ public class NetworkManager : MonoBehaviour
 
         public ProjectilesPacket(List<ProjectileController.Projectile> projectileList)
         {
+            _type = PacketType.PROJECTILE;
+
             MemoryStream projectileMStream = new();
             BinaryWriter projectileBWriter = new(projectileMStream);
+
+            projectileBWriter.Write(_type.ToString());
 
             projectileBWriter.Write(projectileList.Count);
 
@@ -132,10 +145,12 @@ public class NetworkManager : MonoBehaviour
             _data = projectileMStream.ToArray();
         }
 
-        public ProjectilesPacket(byte[] data, Vector3 spawnpos)
+        public ProjectilesPacket(byte[] data)
         {
             MemoryStream projectileMStream = new(data);
             BinaryReader projectileBReader = new(projectileMStream);
+
+            _type = (PacketType)projectileBReader.Read();
 
             int listSize = projectileBReader.ReadInt32();
 
@@ -155,8 +170,8 @@ public class NetworkManager : MonoBehaviour
         }
 
         private byte[] _data;
-        private Vector3 _spawnpos;
-        private List<MockProjectile> _netprojectiles;
+        private readonly List<MockProjectile> _netprojectiles;
+        private readonly PacketType _type;
 
         public List<MockProjectile> GetNetProjectiles()
         { return _netprojectiles; }
@@ -201,9 +216,25 @@ public class NetworkManager : MonoBehaviour
             else recv = socket.ReceiveFrom(data, ref remote);
 
             if (recv == 0) continue;
+            MemoryStream packet = new(data);
+            BinaryReader BReader = new(packet);
 
-            PlayerPacket packet = new(data, gameManager.GetRemote(), playerManager.GetRemote());
-            playerManager.SetNetPosition(packet.GetPosition());
+            switch ((PacketType)BReader.ReadSingle())
+            {
+                case PacketType.PLAYER:
+                    PlayerPacket PlPacket = new(data, gameManager.GetRemote(), playerManager.GetRemote());
+                    playerManager.SetNetPosition(PlPacket.GetPosition());
+                    break;
+
+                case PacketType.PROJECTILE:
+                    ProjectilesPacket PrPacket = new(data);
+                    ModifyProjectilesWithNetInfo(PrPacket.GetNetProjectiles());
+                    break;
+            }
         }
+    }
+
+    private void ModifyProjectilesWithNetInfo(List<ProjectilesPacket.MockProjectile> mockProjectiles)
+    {
     }
 }
