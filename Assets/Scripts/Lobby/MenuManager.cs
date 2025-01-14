733e8aaf97f2606d7d54b5fcc63d5999421c51e2
Júlia Serra Trujillo <juliaserratrujillo@gmail.com>
2025-01-14 22:52:23 +0100

    Added tutorial to menu

--- Assets/Scripts/Lobby/MenuManager.cs
+++ Assets/Scripts/Lobby/MenuManager.cs
@@ -6,24 +6,28 @@ using UnityEngine.UI;
 public class MenuManager : MonoBehaviour
 {
     [SerializeField]
-    private GameObject JoinParent, BaseParent, CreateParent;
+    private GameObject JoinParent, BaseParent, CreateParent, TutorialParent;
 
     [SerializeField]
-    private Button CreateLobbyButton, JoinLobbyButton, QuitButton,
-        CreateLobbyBackButton, JoinLobbyBackButton;
+    private Button CreateLobbyButton, JoinLobbyButton, QuitButton, TutorialButton,
+        CreateLobbyBackButton, JoinLobbyBackButton, TutorialBackButton;
 
     private void Start()
     {
         CreateLobbyButton.onClick.AddListener(() => ChangeMenu(BaseParent, CreateParent));
         JoinLobbyButton.onClick.AddListener(() => ChangeMenu(BaseParent, JoinParent));
+        TutorialButton.onClick.AddListener(() => ChangeMenu(BaseParent, TutorialParent));
+
         CreateLobbyBackButton.onClick.AddListener(() => ChangeMenu(CreateParent, BaseParent));
         JoinLobbyBackButton.onClick.AddListener(() => ChangeMenu(JoinParent, BaseParent));
+        TutorialBackButton.onClick.AddListener(() => ChangeMenu(TutorialParent, BaseParent));
 
         QuitButton.onClick.AddListener(QuitApp);
 
         BaseParent.SetActive(true);
         CreateParent.SetActive(false);
         JoinParent.SetActive(false);
+        TutorialParent.SetActive(false);
     }
 
     private void ChangeMenu(GameObject from, GameObject to)
